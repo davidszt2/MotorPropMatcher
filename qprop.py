3c71d877476bc5177e360dab5fdf28315a04f729
David Moeller Sztajnbok <david.sztajnbok@gmail.com>
2025-01-22 18:36:13 -0800

    Working Ta/Tr

--- qprop.py
+++ qprop.py
@@ -1,6 +1,7 @@
 import numpy as np
 import subprocess
 from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
 import os
 
 class QPROP():
@@ -53,10 +54,24 @@ class QPROP():
         
         sol = fsolve(f, 5000)
         return sol[0]
-        
+    
+    
+    def thrustAvailableSweep(self, VelArr, cellCount):
+        # cellCount: integer count of battery cells, assumed each at 3.7V
+        maxRPM = 3.7 * cellCount * self.kv
+    
+        thrustArr = []
+        ampArr = []
+        for Vel in VelArr:
+            self.run(Vel, maxRPM)
+            thrustArr.append(self.parsedOutput['T(N)'])
+            ampArr.append(self.parsedOutput['Amps'])
+            
+        return thrustArr, ampArr
 
 
 if __name__ == "__main__":
+    """Motor/Prop Sweep"""
     props = os.listdir("Props")
     motors = os.listdir("Motors")
     Treq = 10 # N
@@ -64,15 +79,52 @@ if __name__ == "__main__":
     outputName = "output.csv"
     
     for prop in props:
-        for motor in motors:
-            qprop = QPROP("Props/" + prop, "Motors/" + motor)
-            trimRPM = qprop.convergeThrust(V, Treq)
-            qprop.run(V, trimRPM)
-            output = qprop.parsedOutput
-            thrust = output['T(N)']
-            # print(thrust)
-            
-            # Append motor, prop, trim RPM, and Pelec to CSV
-            with open("output.csv", "a") as f:
-                f.write(motor + "," + prop + "," + str(trimRPM) + "," + str(output['Pelec']) + "," + str(thrust) + "\n") # if np.abs(thrust - Treq) < 0.1 else None
-            
+        motor = "FlightLine_5055_390.txt"
+        qprop = QPROP("Props/" + prop, "Motors/" + motor)
+        trimRPM = qprop.convergeThrust(V, Treq)
+        qprop.run(V, trimRPM)
+        output = qprop.parsedOutput
+        thrust = output['T(N)']
+        
+        # Append motor, prop, trim RPM, and Pelec to CSV
+        with open("output.csv", "a") as f:
+            f.write(motor + "," + prop + "," + str(trimRPM) + "," + str(output['Pelec']) + "," + str(thrust) + "\n") # if np.abs(thrust - Treq) < 0.1 else None
+
+    """Vmax and Current Draws"""
+    motor = "Motors/FlightLine_5055_390.txt"
+    props = ["Props/apce_11x8.txt", "Props/apce_11x10.txt", "Props/apce_12x8.txt", "Props/apce_14x12.txt", "Props/apce_17x12.txt"]
+    labels = ["11x8", "11x10", "12x8", "14x12", "17x12"]
+    velArr = np.linspace(0, 30, 100)
+    cellCount = 6
+    
+    # 2 figures: thrust vs velocity, thrust vs current
+    fig1, ax1 = plt.subplots()
+    fig2, ax2 = plt.subplots()
+    
+    for prop in props:
+        qprop = QPROP(prop, motor)
+        thrustArr, ampArr = qprop.thrustAvailableSweep(velArr, cellCount)
+        ax1.plot(velArr, thrustArr, label=labels[props.index(prop)], linestyle='--')
+        ax2.plot(ampArr, thrustArr, label=labels[props.index(prop)], linestyle='--')
+    
+    CD0 = 0.036 * 2
+    e = 0.96
+    AR = 10
+    k = 1 / (np.pi * e * AR)
+    rho = 1.225
+    S = 0.929
+    Treq = 0.5 * rho * S * CD0 * np.float_power(velArr, 2) + 2 * k * np.float_power(velArr, 2)
+    ax1.plot(velArr, Treq, label="Required", linestyle='-', color='black')
+    
+    fig1.suptitle("Thrust vs Velocity")
+    ax1.set_xlabel("Velocity (m/s)")
+    ax1.set_ylabel("Thrust (N)")
+    ax1.legend()
+    
+    fig2.suptitle("Thrust vs Current")
+    ax2.set_xlabel("Current (A)")
+    ax2.set_ylabel("Thrust (N)")
+    ax2.legend()
+    
+    plt.show()
+    
